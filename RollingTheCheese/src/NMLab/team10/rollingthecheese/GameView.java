b9354c7aec2e9c6018b778fb6d54f1821a91ccaa
bahh723@gmail.com <bahh723@gmail.com@66f13f58-828a-69f6-dd90-9242c65b8804>
2011-05-01 18:54:04 +0000

    scroll version 1

--- RollingTheCheese/src/NMLab/team10/rollingthecheese/GameView.java
+++ RollingTheCheese/src/NMLab/team10/rollingthecheese/GameView.java
@@ -1,235 +1,219 @@
 package NMLab.team10.rollingthecheese;
 
-
-import android.R.bool;
-import android.R.integer;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-
-public class GameView extends SurfaceView implements SurfaceHolder.Callback{
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     GameDrawThread gameDrawThread;
     Bitmap backgroundBitmap;
+    Bitmap buttomBitmap;
     RollingCheeseActivity father;
-    Scroll scroll;
-    int posX;
-    int prevFingerX;
-    long prevTime;
-    private class Scroll extends Thread{
-        private static final int leftEdge = -800;
-        private static final int rightEdge = 0;
-        private static final int quantizeConst = 40; 
-        
-        double V;
-        double A;
-        
-        
-        
-        public Scroll(){
-                
-        }
-        
-        public void run(){
-            prevTime = System.nanoTime();
-            while(true){
-                
-                try {
-                    sleep(30);
-                } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        }
+
+    Lock lock;
+
+    private class Lock {
+
     }
-    
-    /*
+
     Scroll scroll;
-    private class Scroll{
+
+    private class Scroll extends Thread {
         private static final int leftEdge = -800;
         private static final int rightEdge = 0;
-        private static final int quantizeConst = 40; 
-            
-             
+        private static final int quantizeConst = 40;
 
         public boolean isPressing;
         public boolean prev_isPressing;
         public boolean isRecovering;
-        
+
         public int posX;
         public double V;
         public int fingerX;
         public int prev_fingerX;
         public double recover_a;
         public long prev_time;
-        
-        
-        public Scroll(){
+
+        public Scroll() {
             posX = 0;
             prev_isPressing = false;
             isPressing = false;
             isRecovering = false;
-            recover_a=0;
+            recover_a = 0;
             V = 0;
             fingerX = 0;
             prev_fingerX = 0;
             prev_time = 0;
-            
+
         }
-        
-        private int decadeDrag(){
-            if(posX>rightEdge + 3*quantizeConst||posX<leftEdge - 3*quantizeConst){
-                return 0;   
-            //}else if(posX>rightEdge + 2*quantizeConst||posX<leftEdge - 2*quantizeConst){
-             //   return (fingerX-prev_fingerX)/5;
-            //}else if(posX>rightEdge + 1*quantizeConst||posX<leftEdge - 1*quantizeConst){
-             //   return (fingerX-prev_fingerX)/3;
-            }else{
-                return (fingerX-prev_fingerX)/10;
+
+        private int decadeDrag() {
+            if (posX > rightEdge + 3 * quantizeConst
+                    || posX < leftEdge - 3 * quantizeConst) {
+                return 0;
+                // }else if(posX>rightEdge + 2*quantizeConst||posX<leftEdge -
+                // 2*quantizeConst){
+                // return (fingerX-prev_fingerX)/5;
+                // }else if(posX>rightEdge + 1*quantizeConst||posX<leftEdge -
+                // 1*quantizeConst){
+                // return (fingerX-prev_fingerX)/3;
+            } else {
+                return (fingerX - prev_fingerX) / 10;
             }
         }
-        public int doCalc(){
-            //Log.e("scroll posX",String.format("%d", posX));
-            //long cur_time = System.nanoTime();
-            long delta_time = 30000;//cur_time - prev_time;
-            //prev_time = cur_time;
-            if(isPressing){
-                if(prev_isPressing == false){      // run when finger down
-                    prev_isPressing = true;                   
-                    prev_fingerX = fingerX;
-                    isRecovering = false;
-                    recover_a = 0;
-                    V = 0;
-                    return posX;
-                }else{                             //run when move
-                    if((posX>rightEdge && fingerX-prev_fingerX > 0)
-                     || (posX<leftEdge && fingerX-prev_fingerX < 0)){
-                        posX+= decadeDrag();
-                        
-                    }else{
-                        posX+= (fingerX - prev_fingerX);
-                    
+
+        public void run() {
+            // Log.e("scroll posX",String.format("%d", posX));
+            long cur_time;
+            long delta_time;
+            while (true) {
+                synchronized (lock) {
+
+                    cur_time = System.nanoTime();
+                    delta_time = cur_time - prev_time;
+                    prev_time = cur_time;
+                    if (isPressing) {
+                        if (prev_isPressing == false) { // run when finger down
+                            prev_isPressing = true;
+                            prev_fingerX = fingerX;
+                            isRecovering = false;
+                            recover_a = 0;
+                            V = 0;
+                        } else { // run when move
+                            if ((posX > rightEdge && fingerX - prev_fingerX > 0)
+                                    || (posX < leftEdge && fingerX
+                                            - prev_fingerX < 0)) {
+                                posX += decadeDrag();
+
+                            } else {
+                                posX += (fingerX - prev_fingerX);
+
+                            }
+
+                            V = (double) (fingerX - prev_fingerX)
+                                    / (double) delta_time;
+                            if (V > 0.0000002)
+                                V = 0.0000002;
+
+                            prev_fingerX = fingerX;
+
+                        }
+                    } else { // run when finger up
+                        prev_isPressing = false;
+                        posX += V * delta_time;
+                        if (!isRecovering) {
+                            if (posX > rightEdge && V > 0) {
+                                V = -0.000000004 * (double) (posX - rightEdge);
+                                recover_a = 0.000000000000000008 * (double) (posX - rightEdge);
+                                isRecovering = true;
+                            } else if (posX < leftEdge && V < 0) {
+                                V = -0.000000004 * (double) (posX - leftEdge);
+                                recover_a = 0.000000000000000008 * (double) (posX - leftEdge);
+                                isRecovering = true;
+                            }
+                        }
+
+                        posX += (V * delta_time) + 0.5 * recover_a * delta_time
+                                * delta_time;
+                        if (V * (V + recover_a * delta_time) < 0) {
+                            isRecovering = false;
+                            recover_a = 0;
+                            V = 0;
+                        } else {
+                            V += recover_a * delta_time;
+                        }
+
                     }
-                       
-                    V = (double)(fingerX - prev_fingerX)/(double)delta_time;
-                    if(V>0.0000004)V=0.0000004; 
-                       
-                    
-                    prev_fingerX = fingerX;
-                    return posX;
                 }
-            }else{                                 //run when finger up
-                prev_isPressing = false;
-                posX += V*delta_time;
-                if(!isRecovering){
-                    if(posX>rightEdge&&V>0 ){
-                        V = -0.000000004 * (double)(posX-rightEdge);
-                        recover_a = 0.000000000000000008 * (double)(posX-rightEdge);
-                        isRecovering = true;
-                    }else if(posX<leftEdge&&V<0){
-                        V = -0.000000004 * (double)(posX-leftEdge);
-                        recover_a = 0.000000000000000008 * (double)(posX-leftEdge);
-                        isRecovering = true;
-                    }
+                // prev_time = cur_time;
+                // Log.e("Scroll",String.format("p:%d", posX));
+                // return posX;
+                try {
+                    Thread.sleep(30);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                
-                posX+=(V*delta_time)+0.5*recover_a*delta_time*delta_time;
-                if(V*(V+recover_a*delta_time)<0){
-                    isRecovering = false;
-                    recover_a = 0;
-                    V=0;
-                }else{                     
-                    V+=recover_a*delta_time;
-                }
-                
-                return posX;
             }
-            //prev_time = cur_time;
-            //Log.e("Scroll",String.format("p:%d", posX));
-            //return posX;
-            
+
         }
     }
-    */
-    public GameView(RollingCheeseActivity father){
+
+    public GameView(RollingCheeseActivity father) {
         super(father);
         this.father = father;
         getHolder().addCallback(this);
         initBitmap(father);
         gameDrawThread = new GameDrawThread(this, getHolder());
+        lock = new Lock();
         scroll = new Scroll();
-       
-    }
-    
-    public void initBitmap(Context context){
-        Resources r = context.getResources();
-        backgroundBitmap = BitmapFactory.decodeResource(r, R.drawable.cheesegame);
-        
-    }
-    public void doDraw(Canvas canvas){
+        scroll.start();
 
-        //int tranX = scroll.doCalc();
-        //Log.e("",String.format("%d ", tranX));
-        canvas.translate(posX, 0);
-        canvas.drawBitmap(backgroundBitmap, 0,0, null);
-       
-        
     }
-    
-    public void surfaceChanged(SurfaceHolder holder,int format,int width,int height){}
-    public void surfaceCreated(SurfaceHolder holder){
-        if(!father.gameThread.isAlive()){
+
+    public void initBitmap(Context context) {
+        Resources r = context.getResources();
+        backgroundBitmap = BitmapFactory.decodeResource(r,
+                R.drawable.cheesegame);
+        buttomBitmap = BitmapFactory.decodeResource(r, R.drawable.buttom);
+    }
+
+    public void doDraw(Canvas canvas) {
+
+        // int tranX = scroll.doCalc();
+        // Log.e("",String.format("%d ", tranX));
+        synchronized (lock) {
+
+            canvas.translate(scroll.posX - 160, 0);
+            canvas.drawBitmap(backgroundBitmap, 0, 0, null);
+            canvas.translate(-scroll.posX + 160, 0);
+            canvas.drawBitmap(buttomBitmap, 5, 10, null);
+        }
+
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (!father.gameThread.isAlive()) {
             father.gameThread.isRunning = true;
             father.gameThread.start();
         }
-        if(!gameDrawThread.isAlive()){         
+        if (!gameDrawThread.isAlive()) {
             gameDrawThread.isRunning = true;
             gameDrawThread.start();
         }
-        
+
     }
+
     public void surfaceDestroyed(SurfaceHolder holder) {
-        if(gameDrawThread.isAlive()){             
+        if (gameDrawThread.isAlive()) {
             gameDrawThread.isRunning = false;
-      
+
         }
-        if(father.gameThread.isAlive()){             
+        if (father.gameThread.isAlive()) {
             father.gameThread.isRunning = false;
-        }           
+        }
     }
-     
-    
+
     @Override
-    public boolean onTouchEvent(MotionEvent event){
-        int x = (int)event.getX();
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
-            scroll.stop();
-            prevFingerX = x;
-            prevTime = System.nanoTime();
-           // scroll.isPressing = true;
-            //scroll.fingerX = x;
-        }else if(event.getAction() == MotionEvent.ACTION_MOVE){
-            prevTime = System.nanoTime();
-            posX += x - prevFingerX;                        
-            prevFingerX = x;
-            
-            //scroll.fingerX = x;
-        }else if(event.getAction() == MotionEvent.ACTION_UP){
-            long deltaT = System.nanoTime() - prevTime;
-            scroll.V = (double)(x - prevFingerX)/(double)deltaT;
-            //scroll.isPressing = false;
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            scroll.isPressing = true;
+            scroll.fingerX = x;
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            scroll.fingerX = x;
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            scroll.isPressing = false;
         }
         return true;
     }
-    
-    
-    
-    
+
 }
