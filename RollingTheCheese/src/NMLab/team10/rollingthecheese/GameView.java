32df6d61dd799f596246754b6a8f454fd3e64106
occqoo@gmail.com <occqoo@gmail.com@66f13f58-828a-69f6-dd90-9242c65b8804>
2011-05-04 15:42:07 +0000

    background v2

--- RollingTheCheese/src/NMLab/team10/rollingthecheese/GameView.java
+++ RollingTheCheese/src/NMLab/team10/rollingthecheese/GameView.java
@@ -60,7 +60,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         canvas.drawBitmap(backgroundBitmap, newX/2-80,0,null);
         canvas.drawBitmap(farmBitmap, newX/2-80,0,null);
         canvas.drawBitmap(houseBitmap, newX - 160+60,200,null);
-        canvas.drawBitmap(wood_slideBitmap, newX -160+50,275, null);
+        canvas.drawBitmap(wood_slideBitmap, newX -160+60,275, null);
         canvas.drawBitmap(grassBitmap, newX-160,430, null);
         canvas.drawBitmap(buttomBitmap, 5, 10, null);
 
